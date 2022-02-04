da600f15a213b6d2154db23d3fe428e2a5979497
ViktorVx <petrov.v.anatolyev@sberbank.ru>
2022-02-04 17:44:14 +0300

    Add endless console input

--- src/main/java/org/pva/familytreecli/FamilyTreeCliApplication.java
+++ src/main/java/org/pva/familytreecli/FamilyTreeCliApplication.java
@@ -1,13 +1,33 @@
 package org.pva.familytreecli;
 
+import org.springframework.boot.Banner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
+import static java.lang.System.exit;
+
 @SpringBootApplication
-public class FamilyTreeCliApplication {
+public class FamilyTreeCliApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FamilyTreeCliApplication.class, args);
+		SpringApplication app = new SpringApplication(FamilyTreeCliApplication.class);
+		app.setBannerMode(Banner.Mode.OFF);
+		app.run(args);
 	}
 
+	@Override
+	public void run(String... args) {
+		var scanner = new Scanner(System.in);
+		var info = "";
+		while (true) {
+			info = scanner.nextLine();
+			if (info.equals("exit")) {
+				exit(0);
+			}
+			System.out.println(info);
+		}
+	}
 }
